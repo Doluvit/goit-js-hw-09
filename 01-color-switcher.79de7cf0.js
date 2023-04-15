eae97aba448f08cd715de08680ccf0d1a5b4b6e5
Doluvit <117816773+Doluvit@users.noreply.github.com>
2023-04-15 06:39:34 +0000

    Deploying to gh-pages from @ Doluvit/goit-js-hw-09@a1b6ee53fd442b2a08387b46a0791f1a3eaf3ce1 🚀

---
+++ 01-color-switcher.79de7cf0.js
@@ -0,0 +1,2 @@
+!function(){var t={body:document.querySelector("body"),start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]")};t.start.addEventListener("click",(function(){e=setInterval((function(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.start.disabled=!0,t.stop.disabled=!1})),t.stop.addEventListener("click",(function(){clearInterval(e),t.start.disabled=!1,t.stop.disabled=!0})),t.stop.disabled=!0;var e=null}();
+//# sourceMappingURL=01-color-switcher.79de7cf0.js.map
